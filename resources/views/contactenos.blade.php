85a3a0a499eee93b171fac524e61653012a82474
liann-melanny-huaman-cunyas <melanycunyas@gmail.com>
2023-06-30 23:58:19 -0500

    css

--- resources/views/contactenos.blade.php
+++ resources/views/contactenos.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>24deJunio</title>
 </head>
