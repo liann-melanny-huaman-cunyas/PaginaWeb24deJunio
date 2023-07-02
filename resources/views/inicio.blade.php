cb8c30812740fa751c74dc11fb03ff264eaeb625
liann-melanny-huaman-cunyas <melanycunyas@gmail.com>
2023-07-02 13:12:40 -0500

    pp

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('assets/app-ed70b007.css') }}">
+    <script src="{{ asset('assets/app-6870bb4e.js') }}"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>24deJunio</title>
 </head>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+  base: process.env.ASSET_URL || '/',
   plugins: [
     laravel({
       input: [
@@ -15,4 +16,5 @@ export default defineConfig({
     manifest: true, // Generar un archivo de manifiesto para la carga de recursos
     outDir: 'public/build', // Carpeta de salida para los archivos compilados en producción
   },
+
 });
